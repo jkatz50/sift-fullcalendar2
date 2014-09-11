a9385769dba31b477283d5026924fa8b2d852402
J-Katzen <alto50@gmail.com>
2014-09-11 13:50:47 -0400

    new version for new meteor

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
-    summary: "Full-sized drag and drop event calendar (jQuery plugin)"
+    summary: "Full-sized drag and drop event calendar (jQuery plugin)",
+  version: "1.0.8",
+  git: "https://github.com/J-Katzen/sift-fullcalendar2.git"
 });
 
 Package.on_use(function (api) {
-    api.use('jquery-ui', 'client');
+  api.versionsFrom("METEOR@0.9.0");
+    api.use("mrt:jquery-ui@1.9.2", 'client');
     api.add_files([
         'css/fullcalendar.css',
         'lib/fullcalendar.min.js',
